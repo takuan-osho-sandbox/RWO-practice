24cdf019718f1ed32bb02b56191c7f4eda652102
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-10-08 19:45:11 +0900

    Add a basic sample program about variants

---
+++ chap05/basiccolor.ml
@@ -0,0 +1,10 @@
+open Core_kernel
+type basic_color =
+  | Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
+
+let basic_color_to_int = function
+  | Black -> 0 | Red     -> 1 | Green -> 2 | Yellow -> 3
+  | Blue  -> 4 | Magenta -> 5 | Cyan  -> 6 | White  -> 7
+
+
+List.map ~f:basic_color_to_int [Blue; Red]
