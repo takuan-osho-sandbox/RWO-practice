36fb798ad83cecb6201a7245d3750ca29f1f9020
SHIMIZU Taku <shimizu.taku@gmail.com>
2020-08-06 16:17:03 +0900

    Add some fix at a program in chapter 02

--- chap02/part31.ml
+++ chap02/part31.ml
@@ -1,9 +1,10 @@
-open Core
+open Stdio
+open Core_kernel
 
 let (|>) x f = f x
 
 let path = "/usr/bin:/usr/local/bin:/bin:/sbin";;
 
 String.split ~on:':' path
-|> List.dedup ~compare:String.compare
+|> List.dedup_and_sort ~compare:String.compare
 |> List.iter ~f:print_endline
